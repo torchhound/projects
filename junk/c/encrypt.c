d375b609746f93794ef35601f7a7e7a2d13ba6c4
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2015-06-26 21:02:25 -0400

    work in progress

---
+++ junk/c/encrypt.c
@@ -0,0 +1,19 @@
+<include> #stdio.h
+
+char e "The dance of your people.";
+char *e_ptr = &e;
+
+int encrypt (char e) 
+{
+	for (e = 0; e != NULL; *e++)
+}
+
+int decrypt (char e)
+{
+
+}
+
+void main()
+{
+
+}

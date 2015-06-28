441be8430a2e4b93a0ecc3e7b4ce45ab5cffca7c
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2015-06-28 12:54:37 -0400

    added .gitignore, modified textgame1.py and encrypt.c

--- junk/python/textgame1.py
+++ junk/python/textgame1.py
@@ -11,9 +11,14 @@ def map():
 name = str(raw_input("Who are you?: "))
 print "Welcome",name,"."
 print "You are standing on a bluff overlooking a city."
+print "Into the city, back into the forest, or into an interesting cave?"
 action = str(raw_input("Where do you go?: "))
-if action == city:
-	print "You make your way into the city"
+if action == "city":
+	print "You make your way into the city."
+elif action == "forest":
+	print "You fall back into the trees."
+elif action == "cave":
+	print "You enter the mouth of the cave."
 else:
 	print "That city looks mighty inviting!"
 

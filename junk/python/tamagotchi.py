677f1e4888ad0c9543fab53db1851344141decb4
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2015-07-05 15:15:49 -0400

    Fixed some classes and added new functions

--- junk/python/tamagotchi.py
+++ junk/python/tamagotchi.py
@@ -1,10 +1,11 @@
 import random
 
 class creature(object):
-	def __init__(self, name, image, stats):
+	def __init__(self, name,):
         self.name = name
-	self.image = []
-	self.stats = {}
+	def qualities(image, stats):
+		self.image = []
+		self.stats = {}
 
 class tamagotchi(creature):
 	def __init__(self, soiled, egg):
@@ -16,7 +17,8 @@ def game():
 	game_status = True
 	name = raw_input("Please name your pet: ")
 	while game_status = True:
-		pet = tamagotchi(%s, "O", 'hp:5', 'level:1', 'xp:1', 'hunger:5', 'intelligence:5', 'strength:5', 'happiness:5', 'boredom:5', name) #fix classes and inheritance
+		pet = tamagotchi(%s, name) #fix classes and inheritance
+		#"O", 'hp':'5', 'level':'1', 'xp':'1', 'hunger':'5', 'intelligence':'5', 'strength':'5', 'happiness':'5', 'boredom':'5'
 		for x in 3:
 			input = raw_input("MENU \n 1.Stats \n 2.Feed \n 3.Play \n 4.Clean Up 5.Evolve")
 			if input == 1:
--- junk/python/textgame1.py
+++ junk/python/textgame1.py
@@ -1,24 +1,40 @@
-#import pygame
-def help():
-	help = "Available commands: grab, forward, up, down, left, right, backward, examine"
-	if action == "help":
-		print help
-	else:
-		return null
-def map():
-	pass
-#character creation?
-name = str(raw_input("Who are you?: "))
-print "Welcome",name,"."
-print "You are standing on a bluff overlooking a city."
-print "Into the city, back into the forest, or into an interesting cave?"
-action = str(raw_input("Where do you go?: "))
-if action == "city":
-	print "You make your way into the city."
-elif action == "forest":
-	print "You fall back into the trees."
-elif action == "cave":
-	print "You enter the mouth of the cave."
-else:
-	print "That city looks mighty inviting!"
+import random, os #numpy for arrays? pygame for graphics?
 
+def help():
+	print "Available commands: grab, forward, up, down, left, right, backward, examine"
+	
+def map():
+	matrix = [[0 for x in range(5)] for x in range(5)] 
+	
+def character():
+	name = str(raw_input("Who are you?: "))
+
+def game():	
+	print "Welcome",name,"." #need to make this a global variable, maybe stored in an external file?
+	print "You are standing on a bluff overlooking a city."
+	print "Into the city, back into the forest, or into an interesting cave?"
+	action = str(raw_input("Where do you go?: "))
+	if action == "city":
+		print "You make your way into the city."
+	elif action == "forest":
+		print "You fall back into the trees."
+	elif action == "cave":
+		print "You enter the mouth of the cave."
+	else:
+		print "That city looks mighty inviting!"
+		
+def main():
+	game = True
+	while game == True:
+		print "Super Text Game Menu! \n Create Character \n Begin \n Help \n Exit \n"
+		player_input = raw_input("Enter a command: ")
+		if player_input = "Help":
+			help()
+		elif player_input = "Create Character":
+			character()
+		elif player_input = "Begin":
+			game()
+		elif player_input = "Exit":
+			os.exit()
+		else:
+			null

f71d2dda1272e881a0e9b8418b72365075ae3a57
torchhound <unknownmasayoshi@gmail.com>
2015-07-10 21:06:46 -0400

    added js

---
+++ junk/js/practice/practice.js
@@ -0,0 +1,17 @@
+z = 3;
+z += 6;
+window.alert(z);
+confirm("Are you a human?");
+var h = prompt("Please enter a valid integer:")
+if (h === parseInt(h,10))
+{
+	confirm("Good work");
+}
+else if (h != parseInt(h,10))
+{
+	confirm("You fool!");
+}
+else()
+{
+	break;
+}

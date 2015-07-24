1e7977cf53f5437de3c82aeb8b1ba1230ba6f8d3
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2015-07-24 18:35:31 -0400

    syntax and whitespace fixes

--- junk/haskell/pomodoro.hs
+++ junk/haskell/pomodoro.hs
@@ -1,23 +1,36 @@
 import Data.Time
 
-main = do
-	work = 25
-	break = 5
-	breakOccur = False
-	start = getCurrentTime
-	stopWork = start + 25
-	stopBreak = start + 5
-	if start = stopWork
-		then 
-			"Time for a break!" 
-			breakOccur = True 
-			main
-				if breakOccur = True
-					then
-						if start = stopBreak
+timer = do
+			let work = 25
+				break = 5
+				breakOccur = False
+				start = getCurrentTime
+				stopWork = start + 25
+				stopBreak = start + 5
+			if start == stopWork
+				then 
+					"Time for a break!" 
+					let breakOccur = True
+					do
+						timer
+					if breakOccur == True
 							then
-								"Get back to work."
-								breakOccur = False
-								main
-				else main
-	else main \n
+								if start == stopBreak
+									then
+										"Get back to work."
+										let breakOccur = False
+										do
+											timer
+								else 
+									do
+										timer
+					else 
+						do
+							timer
+			else 
+				do
+					timer \n
+
+main = do
+		timer \n
+	

210524deb045bf417750123280256601a81c43c8
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2015-08-20 12:41:56 -0400

    added utility bash scripts for Unnamed Game

---
+++ junk/bash/eolComma.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Enter path to folder: "
+read folder
+cd folder
+for file in *.csv
+do
+	set lineCount = wc -l < $file
+	lineCount - 1
+	for line in lineCount
+	do
+		awk -F, -v OFS=, '{$9 = ",,"};1' file 
+	done
+done
---
+++ junk/bash/pngStrip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Enter complete path to folder: "
+read folder
+cd folder
+for file in *.png
+do
+	convert -strip $file
+done

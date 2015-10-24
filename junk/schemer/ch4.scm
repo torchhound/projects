d681abd7bcb0d9d659273594cc59a4c2e4805d22
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2015-10-23 21:58:25 -0400

    added ch4

---
+++ junk/schemer/ch4.scm
@@ -0,0 +1,32 @@
+(define add1
+	(lambda (n)
+		(+ n 1)
+	)
+)
+
+(define sub1
+	(lambda (n)
+		(- n 1)
+	)
+)
+
+(define plus
+	(lambda (x y)
+		(cond ((zero? y) x)
+			  (else (add1 (+ x (sub1 y))))
+		)
+	)
+)
+
+(define minus
+	(lambda (x y)
+		(cond ((zero? y) x)
+			  (else (sub1 (- x (sub1 y))))
+		)
+	)
+)
+
+(define addTup
+	(lambda (tup)
+		(cond ((null? tup) 0)
+			  (

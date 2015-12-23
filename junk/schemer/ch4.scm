adcc3dafe899bed89abb894ea69856881d3f3326
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2015-12-22 19:20:29 -0500

    more work on ch4.scm

--- junk/schemer/ch4.scm
+++ junk/schemer/ch4.scm
@@ -29,7 +29,7 @@
 (define addTup
 	(lambda (tup)
 		(cond ((null? tup) 0)
-			  (else (+ (car tup) (addTup cdr tup)))
+			  (else (plus (car tup) (addTup cdr tup)))
 		)
 	)
 )
@@ -37,7 +37,7 @@
 (define x
 	(lambda (n m)
 		(cond ((zero? m) 0) ;checks if m aka the counter is zero which closes recursion
-				(else (+ n(x n(sub1 m)))) ;adds n to the natural recursion of n where m acts as a counter for the number off additions
+				(else (plus n(x n(sub1 m)))) ;adds n to the natural recursion of n where m acts as a counter for the number off additions
 		)
 	)
 )
@@ -47,12 +47,12 @@
 		(cond ((and (null? tup1)(null? tup2)) (quote ()))
 			  ((null? tup1) tup2) ;simplify?
 			  ((null? tup2) tup1)
-			  (else (cons (+ (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2))))
+			  (else (cons (plus (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2))))
 		)
 	)
 )
 
-(define >
+(define _>
 	(lambda (n m)
 		(cond ((zero? n) #f)
 		      ((zero? m) #t)
@@ -60,3 +60,41 @@
 		)
 	)
 )
+
+(define _<
+	(lambda (n m)
+		(cond ((zero? n) #f)
+		      ((zero? m) #t)
+			  (else (< (sub1 m)(sub1 n))
+		)
+	)
+)
+
+(define _=
+	(lambda (n m)
+		(cond ((> n m) #f)
+			  ((< n m) #f)
+			  (else #t)
+		)
+	)
+)
+
+(define power
+	(lambda (n m)
+		(cond ((zero? m) 1) ;((zero? n) 0) unnecessary
+			  (else (x n(power n(sub1 m)))) ;multiplication with m as a counter
+		)
+	)
+)
+
+(define division ;originally weirdEquality 
+	(lambda (n m)
+		(cond ((< n m) 0)
+			  (else add1 (division (minus n m) m))
+		)
+	)
+)
+
+(define _length
+	(lambda (lat)
+		(cond ((

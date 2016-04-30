960d33b4fe98859f43888b39910e9232fa3ce5b4
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2016-04-30 02:11:57 -0400

    golfed runArray.py down to 110 bytes, working on hTGolf.sh

---
+++ junk/bash/hTGolf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#written for http://codegolf.stackexchange.com/questions/78886/hackertyper-net/78892
+while read -r;do
+read -rsn1 inp #conflicting reads
+if ; then #need a way to check if anything is in inp
+echo "$REPLY"
+fi
+done
--- junk/python/runArray.py
+++ junk/python/runArray.py
@@ -1,13 +1,13 @@
 #written for http://codegolf.stackexchange.com/questions/78845/run-through-an-array
 
-from random import *
-def a(x, y):
-    ptr = 0
-    for z in x:
-        if random() > 0.5:
-            x[ptr] = z + y
-        ptr += 1
-    print(x)
+from random import*
+def a(x,y):
+ ptr=0
+ for z in x:
+  if random()>0.5:
+   x[ptr]=z+y
+  ptr+=1
+ print(x)
         
 def main():
     test = [1, 2, 3, 4, 5]

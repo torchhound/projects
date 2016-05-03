b3ed8e018795dcfb5b805e2d47a8f95f9d64fc91
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2016-05-02 21:36:44 -0400

    fleshed out mergeSort.py but accidentally made a sorting function that isn't mergeSort

--- junk/python/mergeSort.py
+++ junk/python/mergeSort.py
@@ -1,1 +1,24 @@
-def mergerSort(data):
+def split(data):
+	half = len(data)//2
+	return data[:half], data[half:]
+	
+def breakL(data):
+	for x in data:
+		if len(data) == 1:
+			return data
+		else:
+			split(data)
+			
+def sort(*args):
+	size = len(args)
+	old = list(args)
+	new = []
+	for x in size:
+		mx = max(old)
+		mn = min(old)
+		new[0] = mn
+		new.append(mx)
+		mxI = index(mx)
+		mnI = index(mn)
+		del old[mxI]
+		del old[mnI]

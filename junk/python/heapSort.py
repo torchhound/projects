f1384f15491566220a28937bb72917ec55cfe050
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2016-05-06 22:09:05 -0400

    fleshed out heapSort.py

--- junk/python/heapSort.py
+++ junk/python/heapSort.py
@@ -1,1 +1,14 @@
+import heapq
+
 def heapSort(data):
+	left = len(hp) #not sure if len() applies to heaps
+	right = 0
+	hp = heapq.heapify(data)
+	for x in hp:
+		if right < left:
+			heapq.heapreplace(hp, hp[left])
+		else:
+			continue
+		right += 1
+		left -= 1
+	return hp

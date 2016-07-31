e54799e740eb05207c27f82617db2a14570f3cc9
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2016-07-31 18:48:03 -0400

    Added ionic/testApp blank, small fixes for worstServe.py

--- junk/python/worstServe.py
+++ junk/python/worstServe.py
@@ -7,12 +7,12 @@ def chat(port):
 
 
 def webServe(page):
-	call("nc -l -p 80 -q 1 < %s" %page) #might need a keep alive
-	
+	call("nc -l -p 80 -q 1 < {}".format(page)) #might need a keep alive
+
 def main():
 	print("Serves an html page on port 80, only works on Linux")
-	userPage = input("Path to page to serve: ")
+	userPage = input("Path to a page to serve: ")
 	webServe(userPage)
-	
+
 if __name__ == '__main__':
 	main()

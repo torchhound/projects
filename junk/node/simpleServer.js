2074f069232fee6e7d20c68ec46787e097a6689d
torchhound <unknownmasayoshi@gmail.com>
2016-08-03 16:11:18 -0400

    Moved mean and node to webDev, ionic to ionicPractice

--- junk/node/simpleServer.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-
-var server = http.createServer(function(request, response){
-	console.log("Request made");
-	response.write("Welcome!");
-	response.end;
-});
-
-server.listen("3000");

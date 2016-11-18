7bd4390e91d603879d91e941043189f4863a4ecc
torchhound <jcieslik@umail.iu.edu>
2016-11-18 14:47:37 -0500

    Cryptopals challenge 1 done, newline=False not working

--- junk/python/cryptopals.py
+++ junk/python/cryptopals.py
@@ -1,10 +1,11 @@
-from base64 import b64encode
+import binascii
 
 def hexToBase(hx):
-	return b64encode(bytes(hx))
+	raw = binascii.unhexlify(hx)
+	return binascii.b2a_base64(raw)
 
 def main():
-	print(hexToBase(0x49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d))
+	print(hexToBase("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
 
 if __name__ == "__main__":
 	main()

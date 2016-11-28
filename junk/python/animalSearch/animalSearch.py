ee542c58185de60b1e96fcf69f9115ffd0870e9d
torchhound <jcieslik@umail.iu.edu>
2016-11-28 16:41:49 -0500

    Tried to search housemates but need fuzzy homophone search unfortunately the package fuzzy is only compatible with python2.7

--- junk/python/animalSearch/animalSearch.py
+++ junk/python/animalSearch/animalSearch.py
@@ -1,12 +1,21 @@
 def animalSearch(name):
 	f = open("animals.txt", "r")
-	for x in f:
-		if name in x:
-			print(x)
+	count = 0
+	try:
+		for x in f:
+			if name in x:
+				count += 1
+				print(x + " " + str(count))
+	finally:
+		f.close()
 			
 def main():
 	i = input("Enter name to find in an animal's common name: ")
 	animalSearch(i)
+	roommates = ["hannah", "sam", "milo", "tristan", "marie", "melissa", "andy", "gabe", "heather", "sydny", "stephan", "courtney", "brett", "juan", "alex", "anthony", "whitney", "anne", "gina", "joey"]
+	for y in roommates:
+		print(y)
+		animalSearch(y)
 	
 if __name__ == "__main__":
 	main()

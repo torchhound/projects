c48f36efe9fad141e2171662c7c41b958bcbbdda
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2017-01-24 01:18:23 -0500

    added schemer ch2 and began ch3

--- junk/schemer/schemer2rktbugaloo.rkt
+++ junk/schemer/schemer2rktbugaloo.rkt
@@ -20,3 +20,34 @@
 (eq? #f #f)
 (eq? '3.14 '3.14)
 (eq? '3.14 '3.1415)
+(quote (end ch1))
+
+(define lat?
+  (lambda (x)
+    (cond
+      ((null? x) #t)
+      ((atom? (car x)) (lat? (cdr x)))
+      (else #f))))
+
+(lat? '(Bacon and tuna sandwiches))
+
+(define member?
+  (lambda (x lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) x)
+                (member? x (cdr lat)))))))
+
+(member? 'meat '(mashed potatoes and meat gravy))
+(member? 'delicious '(rotten old shoes))
+(quote (end ch2))
+
+(define rember
+  (lambda (x lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) x) (cdr lat))
+      (else (cons (car lat) (rember x (cdr lat)))))))
+
+(rember 'bacon '(bacon lettuce and tomato))
+(rember 'and '(bacon lettuce and tomato))

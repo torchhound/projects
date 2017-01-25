d7485b4941a8e07ba58a2d12fd9239b303f891c3
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2017-01-24 22:51:17 -0500

    Finished schemer ch3

--- junk/schemer/schemer2rktbugaloo.rkt
+++ junk/schemer/schemer2rktbugaloo.rkt
@@ -51,3 +51,118 @@
 
 (rember 'bacon '(bacon lettuce and tomato))
 (rember 'and '(bacon lettuce and tomato))
+
+(define firsts
+  (lambda (lst)
+    (cond
+      ((null? lst) (quote ()))
+      (else (cons (car (car lst)) (firsts (cdr lst)))))))
+
+(firsts '((a list) (of lists) (which contain) (several elements)))
+(firsts '((a b) (c d) (e f)))
+
+(define insertList
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) old) (cons (cons (car lat) new) (cdr lat)))
+      (else (cons (car lat) (insertList new old (cdr lat)))))))
+
+(insertList 'e 'd '(a b c d f g d h))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+       (cond
+         ((eq? (car lat) old) (cons old (cons new (cdr lat))))
+         (else (cons (car lat)
+                     (insertR new old (cdr lat)))))))))
+
+(insertR 'e 'd '(a b c d f g d h))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+       (cond
+         ((eq? (car lat) old) (cons new lat))
+         (else (cons (car lat)
+                     (insertL new old (cdr lat)))))))))
+
+(insertL 'e 'd '(a b c d f g d h))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+       (cond
+         ((eq? (car lat) old) (cons new (cdr lat)))
+         (else (cons (car lat) (subst new old (cdr lat)))))))))
+
+(subst 'e 'i '(h i l l))
+
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+       (cond
+         ((or (eq? (car lat) o1) (eq? (car lat) o2))
+          (cons new (cdr lat)))
+         (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))))
+
+(subst2 'o 'e 'a '(c r e a k))
+(subst2 'o 'e 'a '(c r a e k))
+
+(define multirember
+  (lambda (x lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+       (cond
+         ((eq? (car lat) x) (multirember x (cdr lat)))
+         (else (cons (car lat) (multirember x (cdr lat)))))))))
+
+(multirember 'o '(c r o o k))
+
+(define multiInsertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+       (cond
+         ((eq? (car lat) old)
+          (cons (car lat) (cons new (multiInsertR new old (cdr lat)))))
+         (else (cons (car lat)
+                     (multiInsertR new old (cdr lat)))))))))
+
+(multiInsertR 'e 'o '(c r o o k))
+
+(define multiInsertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+       (cond
+         ((eq? (car lat) old)
+          (cons new (cons old (multiInsertL new old (cdr lat)))))
+         (else (cons (car lat)
+                     (multiInsertL new old (cdr lat)))))))))
+
+(multiInsertL 'e 'o '(c r o o k))
+
+(define multiSubst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+       (cond
+         ((eq? (car lat) old)
+          (cons new (multiSubst new old (cdr lat))))
+         (else (cons (car lat) (multiSubst new old (cdr lat)))))))))
+
+(multiSubst 'e 'o '(c r o o k))

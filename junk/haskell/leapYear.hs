420f075714dd7c7474a96bd7b94f898027d94092
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2017-03-22 17:59:13 -0400

    Added junk/haskell/leapYear.hs working

---
+++ junk/haskell/leapYear.hs
@@ -0,0 +1,11 @@
+isLeapYear :: Integer -> Bool
+isLeapYear year 
+    | mod year 400 == 0 = True
+    | mod year 100 == 0 = False
+    | mod year 4 == 0 = True
+    | otherwise = False
+
+main :: IO ()
+main = do
+	line <- getLine
+	print $ isLeapYear (read line::Integer)

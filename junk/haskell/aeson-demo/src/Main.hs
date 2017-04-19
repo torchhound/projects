c1f71e582974c3db3433be2465521e5056e6d51b
torchhound <unknownmasayoshi@gmail.com>
2017-04-19 18:45:09 -0400

    More work on junk/haskell/aeson-demo, Error in $: expected [a], encountered Object

--- junk/haskell/aeson-demo/src/Main.hs
+++ junk/haskell/aeson-demo/src/Main.hs
@@ -1,9 +1,20 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
 
 module Main where
 
 import Network.HTTP.Conduit (simpleHttp)
 import qualified Data.ByteString.Lazy as BSL
+import Data.Aeson
+import Data.Text
+import GHC.Generics
+import Control.Monad
+import Control.Applicative
+
+data Currency = 
+  Currency { base :: Text
+           , date :: Text
+           , rates :: Array
+           } deriving(Show, Generic, FromJSON)
 
 jsonURL :: String
 jsonURL = "https://api.fixer.io/latest"
@@ -13,5 +24,7 @@ getJSON = simpleHttp jsonURL
 
 main :: IO ()
 main = do
-  jsonOut <- getJSON
-  BSL.putStr jsonOut
+  jsonOut <- (fmap eitherDecode getJSON) :: IO (Either String [Currency])
+  case jsonOut of
+    Left err -> putStrLn err
+    Right ps -> print ps
